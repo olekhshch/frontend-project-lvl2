6e797f9561070c027b9bfe6cf17d2041f883f127
olekhshch <oleh.shchevrovskyi@gmail.com>
2022-02-16 20:58:53 +0100

    Step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command;
 
 program.version('1.0.0');
 program.description('Compares two configuration files and shows a difference.');
+program.arguments('<filepath1> <filepath2>');
+program.option('-f, --format <type>', 'output format');
 
 program.parse();
 
